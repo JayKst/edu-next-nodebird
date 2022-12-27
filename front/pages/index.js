f0a46e633803fd9be6576b4464c4f92d3aa2b582
양진규 <classic@kakao.com>
2022-12-27 21:39:37 +0900

    1-3. next 라우팅 시스템

---
+++ front/pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Link from 'next/link';
+const Home = () =>{
+    return (
+        <>
+            <Link href="/about">about</Link>
+            <div>Hello, jkyang</div>
+        </>
+    )
+}
+
+export default Home;
