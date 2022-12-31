fdef4e2b782b7f06cf12ddacc955c55f5be7497a
양진규 <classic@kakao.com>
2022-12-31 16:29:36 +0900

    1-5. 기본페이지 만들기

--- front/pages/index.js
+++ front/pages/index.js
@@ -1,11 +1,9 @@
 import React from 'react';
-import Link from 'next/link';
 import AppLayout from "../components/AppLayout";
 const Home = () =>{
     return (
         <>
             <AppLayout>
-                <Link href="/about">about</Link>
                 <div>Hello, jkyang</div>
             </AppLayout>
         </>
