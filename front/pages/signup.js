a92dc886eb12e9063ddb529e1970ad3a514bcb97
양진규 <classic@kakao.com>
2022-12-31 17:01:51 +0900

    1-6. 회원가입 폼 만들기

--- front/pages/signup.js
+++ front/pages/signup.js
@@ -1,12 +1,65 @@
-import React from 'react';
+import React, { useState } from 'react';
 import AppLayout from "../components/AppLayout";
+import {Form, Input, Checkbox, Button} from "antd";
 
 const Signup = () => {
+    const [id, setId] = useState('');
+    const [nick, setNick] = useState('');
+    const [password, setPassword] = useState('');
+    const [passwordCheck, setPasswordCheck] = useState('');
+    const [term, setTerm] = useState(false);
+    const onSubmit =()=>{
+
+    }
+    const onChangeId =(e)=>{
+        setId(e.target.value);
+    }
+    const onChangeNick =(e)=>{
+        setNick(e.target.value);
+    }
+    const onChangePassword =(e)=>{
+        setPassword(e.target.value);
+    }
+    const onChangePasswordChk =(e)=>{
+        setPasswordCheck(e.target.value);
+
+    }
+    const onChangeTerm =(e)=>{
+        setTerm(e.target.value);
+    }
+
     return (
     <AppLayout>
-        <div>
-            회원가입
-        </div>
+        <Form onSubmit={onSubmit} style={{padding:10}}>
+            <div>
+                <label htmlFor='user-id'>아이디</label>
+                <br/>
+                <Input name='user-id' value={id} required onChange={onChangeId}/>
+            </div>
+            <div>
+                <label htmlFor='user-nick'>닉네임</label>
+                <br/>
+                <Input name='user-nick' value={nick} required onChange={onChangeNick}/>
+            </div>
+            <div>
+                <label htmlFor='user-password'>비밀번호</label>
+                <br/>
+                <Input name='user-password' value={password} required onChange={onChangePassword}/>
+            </div>
+            <div>
+                <label htmlFor='user-password-chk'>비밀번호 체크</label>
+                <br/>
+                <Input name='user-password-chk' value={passwordCheck} required onChange={onChangePasswordChk}/>
+            </div>
+            <div>
+                <Checkbox name='user-term' value={term} onChange={onChangeTerm}>동의합니다.</Checkbox>
+            </div>
+            <div>
+                <Button type='primary' htmlType='submit'>
+                    가입하기
+                </Button>
+            </div>
+        </Form>
     </AppLayout>
     );
 }
