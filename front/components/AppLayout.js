dc98c0142f14f5a365a4b1ca382f42863267adb6
양진규 <classic@kakao.com>
2023-01-03 19:50:15 +0900

    2-3. antd 그리드 시스템

--- front/components/AppLayout.js
+++ front/components/AppLayout.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import Link from 'next/link';
 import PropTypes from "prop-types";
-import {Button, Input, Menu} from "antd";
+import {Avatar, Button, Card, Col, Input, Menu, Row} from "antd";
 
+const dummy={
+    nickname: '양진규',
+    Post: [],
+    Followings: [],
+    Followers: []
+}
 const AppLayout = ({children}) => {
     return(
     <div>
@@ -26,7 +32,23 @@ const AppLayout = ({children}) => {
                 회원가입
             </Button>
         </Link>
-        {children}
+        <Row>
+            <Col xs={24} md={6}>
+                <Card
+                actions={[
+                    <div key="twit">쨱짹<br/>{dummy.Post.length}</div>,
+                    <div key="following">팔로잉<br/>{dummy.Followings.length}</div>,
+                    <div key="follower">팔로워<br/>{dummy.Followers.length}</div>,
+
+                ]}>
+                    <Card.Meta
+                        avatar={<Avatar>{dummy.nickname[0]}</Avatar>} title={dummy.nickname}
+                    />
+                </Card>
+            </Col>
+            <Col xs={24} md={12}>{children}</Col>
+            <Col xs={24} md={6}>세번째</Col>
+        </Row>
     </div>)
 }
 
